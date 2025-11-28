0d82dc1a3d57993b1b0ce5ae5a72abb60c3721c2
rry125 <A113221029@mail.shu.edu.tw>
2025-11-28 15:03:17 +0800

    1128

---
+++ 1128/Stage3/src/Role.java
@@ -0,0 +1,40 @@
+// 第二階段和第三階段完全相同
+public abstract class Role {
+    private String name;
+    private int health;
+    private int attackPower;
+
+    public Role(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    // getter/setter 方法
+    public String getName() { return name; }
+    public int getHealth() { return health; }
+    public int getAttackPower() { return attackPower; }
+    public void setHealth(int health) { this.health = health; }
+    public boolean isAlive() { return health > 0; }
+
+    // 具體方法
+    public void takeDamage(int damage) {
+        this.health -= damage;
+        System.out.println("💥 " + name + " 受到 " + damage + " 點傷害！目前生命值：" + health);
+        if (!isAlive()) {
+            onDeath();
+        }
+    }
+
+    // 抽象方法
+    public abstract void attack(Role opponent);
+    public abstract void showSpecialSkill();
+    public abstract void onDeath();
+    public abstract void prepareBattle();
+    public abstract void afterBattle();
+
+    @Override
+    public String toString() {
+        return "角色名稱: " + name + ", 生命值: " + health;
+    }
+}
---
+++ 1128/Stage4/src/Healable.java
@@ -0,0 +1,10 @@
+public interface Healable {
+    void heal(Role target);           // 抽象方法
+    int getHealPower();               // 抽象方法
+    default boolean canHeal() {       // 預設方法
+        return getHealPower() > 0;
+    }
+    default void showHealInfo() {     // 預設方法
+        System.out.println("💚 治療力：" + getHealPower());
+    }
+}
